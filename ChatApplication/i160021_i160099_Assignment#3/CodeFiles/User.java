9646e6f7bea02ba0594612e7aac14aa642b71329
rafiaMushtaq <61281438+rafiaMushtaq@users.noreply.github.com>
2020-08-07 21:55:24 +0500

    Create User.java

---
+++ ChatApplication/i160021_i160099_Assignment#3/CodeFiles/User.java
@@ -0,0 +1,12 @@
+package com.zeelawahab.i160021_i160099;
+
+public class User {
+
+    private String email;
+    private String name;
+
+    public User(String e,String n){
+        email=e;
+        name=n;
+    }
+}
